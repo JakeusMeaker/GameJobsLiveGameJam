f82ed8f6563ee647ce755e7f630735d88b625203
Stamperton <35846593+Stamperton@users.noreply.github.com>
2021-02-22 16:35:57 +0000

    Initial Commit
    Initial Commit, I already said.

---
+++ Assets/Scripts/StampScripts/SO_Character.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character", order = 0)]
+public class SO_Character : ScriptableObject
+{
+    public string characterName;
+
+    public E_Trait firstTrait;
+    public E_Trait secondTrait;
+
+    public int stamina = 3;
+    public int health = 3;
+
+    public void InitCharacter()
+    {
+        GenerateName();
+        GenerateTraits();
+        health = 3;
+        stamina = 3;
+
+        Debug.Log(characterName + ", " + firstTrait + ", " + secondTrait);
+    }
+
+    void GenerateName()
+    {
+        int _rFN = System.Enum.GetValues(typeof(E_FirstName)).Length - 1;
+        E_FirstName _fn = (E_FirstName)Random.Range(0, _rFN);
+
+        int _rLN = System.Enum.GetValues(typeof(E_LastName)).Length - 1;
+        E_LastName _ln = (E_LastName)Random.Range(0, _rLN);
+
+        characterName = _fn.ToString() + " " + _ln.ToString();
+    }
+
+    void GenerateTraits()
+    {
+        int _random = System.Enum.GetValues(typeof(E_Trait)).Length - 1;
+        firstTrait = (E_Trait)Random.Range(0, _random);
+        secondTrait = (E_Trait)Random.Range(0, _random);
+        while (firstTrait == secondTrait)
+        {
+            secondTrait = (E_Trait)Random.Range(0, _random);
+        }
+    }
+}
---
+++ Assets/Scripts/StampScripts/enums.cs
@@ -0,0 +1,65 @@
+﻿public enum E_Trait
+{
+    //Physical
+    Strong,
+    Tough,
+    Agile,
+    Limber,
+
+    //Mental
+    Brave,
+    Intelligent,
+    Educated,
+    Logical,
+    //Social
+    Leader,
+    Encouraging,
+    Persuasive
+
+    //Misc
+}
+
+public enum E_FirstName
+{
+    Jake,
+    Richard,
+    Linda,
+    Alice,
+    Merry,
+    Bob,
+    Steve,
+    Kevin,
+    Patricia,
+    Katie,
+    Sam,
+    Sky,
+    Heather,
+    Ian,
+    Charlie,
+    Chaz,
+    Jeff,
+    Joe,
+    Joseph,
+    Sean,
+    Shaun,
+    Shawn,
+    Snerd
+}
+
+public enum E_LastName
+{
+    Jakonson,
+    Stamp,
+    Meaker,
+    Meakin,
+    Shee,
+    Merri,
+    Nguyen,
+    Smith,
+    Dubois,
+    Johnson,
+    Ramsay,
+    Pilkington,
+    Aperds
+}
+
