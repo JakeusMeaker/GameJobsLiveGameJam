0b49d51b1adf8ac5690e8c8f23a0f202e00be38b
Stamperton <35846593+Stamperton@users.noreply.github.com>
2021-02-22 18:05:04 +0000

    CharacterSelect and Main Menu scene kinda active-ish
    Damn I'm good at this, apparently.
    What a fluke.

--- Assets/Scripts/StampScripts/CharacterManager.cs
+++ Assets/Scripts/StampScripts/CharacterManager.cs
@@ -1,18 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterManager : MonoBehaviour
 {
-    public SO_Character[] selectedCharacters = new SO_Character[3];
+    [Header("Setup Variables")]
+    public GameObject characterSelectCanvas;
+    public GameObject mainCharacterCanvas;
 
+    [Header("MainCanvas Variables")]
+    public Text[] traitButtons;
+    public Text[] characterNames;
+
+    public SO_Character[] characterList;
+    public List<SO_Character> selectedCharacters = new List<SO_Character>();
+    public List<E_Trait> selectedTraits = new List<E_Trait>(6);
     private void Start()
     {
-        for (int i = 0; i < selectedCharacters.Length; i++)
+        for (int i = 0; i < characterList.Length; i++)
         {
-            selectedCharacters[i].InitCharacter();
+            characterList[i].InitCharacter();
         }
 
         CharacterSelectionManager.instance.PopulateUICharacters();
+    }
+
+    public void CharacterSelectButton(int buttonPress)
+    {
+        selectedCharacters.Add(characterList[buttonPress]);
+
+        selectedTraits.Add(characterList[buttonPress].firstTrait);
+        selectedTraits.Add(characterList[buttonPress].secondTrait);
+
+        Debug.Log(characterList[buttonPress].characterName);
+
+        if (selectedCharacters.Count == 3)
+        {
+            Debug.Log("CHARACTERS SELECTED");
+            TransitionToGame();
+            // Move Forwards
+        }
+    }
+
+    void TransitionToGame()
+    {
+        characterSelectCanvas.SetActive(false);
+        mainCharacterCanvas.SetActive(true);
+
+        for (int i = 0; i < traitButtons.Length; i++)
+        {
+            traitButtons[i].text = selectedTraits[i].ToString();
+        }
+        for (int i = 0; i < selectedCharacters.Count; i++)
+        {
+            characterNames[i].text = selectedCharacters[i].characterName;
+        }
+    }
+
+    //THIS FUNCTION IS WHERE WE CHOOSE THE SELECTED TRAIT TO USE
+    public void OnTraitButtonPress(int buttonPosition)
+    {
+        int _charNumber = (int)buttonPosition / 2; //This is the Character that used this. Can use this to decrement health/stamina.
+
+        string debugLog = selectedTraits[buttonPosition].ToString() + " was used by " + selectedCharacters[_charNumber].characterName;
+
+        Debug.Log(debugLog);
     }
 }
