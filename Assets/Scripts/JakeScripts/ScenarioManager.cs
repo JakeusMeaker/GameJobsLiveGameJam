2f73f841fa85b9707b334523b26322e72139d558
Jake Meaker <JM219785@falmouth.ac.uk>
2021-02-22 18:47:44 +0000

    Adding Scenario Scriptable object and Scenario manager
    Scenario manager will handle player choices and provide scenario text
    
    When adding scenarios to the scenario queue, it won't chose one that uses a trait from the previous scenario

---
+++ Assets/Scripts/JakeScripts/ScenarioManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class ScenarioManager : MonoBehaviour
+{
+    public int amountOfScenarios;
+
+    [SerializeField]
+    private List<ScenarioSO> scenarioList = new List<ScenarioSO>();
+
+
+    private Queue<ScenarioSO> scenarioQueue = new Queue<ScenarioSO>();
+    private ScenarioSO currentScenario;
+    private E_Trait lastTraidUsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateScenarioQueue();
+        currentScenario = scenarioQueue.Peek();
+    }
+
+    private void GenerateScenarioQueue()
+    {
+        while (scenarioQueue.Count < amountOfScenarios)
+        {
+            ScenarioSO scenario = scenarioList[Random.Range(0, scenarioList.Count)];
+            if (scenarioQueue != null)
+            {
+                if (scenario.traitToPass != lastTraidUsed)
+                {
+                    scenarioQueue.Enqueue(scenario);
+                    lastTraidUsed = scenario.traitToPass;
+                    print(scenario.name + "added to queue");
+                }
+            }
+            else
+            {
+                scenarioQueue.Enqueue(scenario);
+                lastTraidUsed = scenario.traitToPass;
+                Debug.Log("Added " + scenario.name);
+            }
+        }
+    }
+
+    public void NextScenario()
+    {
+        //Fade transition 
+        scenarioQueue.Dequeue();
+        currentScenario = scenarioQueue.Peek();
+    }
+}
---
+++ Assets/Scripts/JakeScripts/ScenarioSO.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Scenario", menuName = "CreateNewScenario", order = 1)]
+public class ScenarioSO : ScriptableObject
+{
+    public string scenarioText;
+
+    public string passText;
+    public string failText;
+
+    public string characterCriticalFail;
+    public string partyCriticalFail;
+
+    public E_Trait traitToPass;
+
+
+    public string Sucess(string characterName)
+    {
+        return characterName + ' ' + passText;
+    }
+
+    public string Fail(string characterName)
+    {
+        return characterName + ' ' + failText;
+    }
+
+    public string CharacterCriticalFail(string characterName)
+    {
+        return characterName + ' ' + characterCriticalFail;
+    }
+
+    public string PartyCriticalFail(string characterName)
+    {
+        return characterName + ' ' + partyCriticalFail;
+    }
+}
