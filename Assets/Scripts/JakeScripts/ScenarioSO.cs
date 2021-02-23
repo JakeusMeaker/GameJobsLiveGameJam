3806ff58249637b88bb11181388322d6eb6fd3ca
Stamperton <35846593+Stamperton@users.noreply.github.com>
2021-02-23 15:34:59 +0000

    Minor Changes

--- Assets/Scripts/JakeScripts/ScenarioSO.cs
+++ Assets/Scripts/JakeScripts/ScenarioSO.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Scenario", menuName = "CreateNewScenario", order = 1)]
 public class ScenarioSO : ScriptableObject
 {
+    [TextArea]
     public string scenarioText;
 
+    [TextArea]
     public string passText;
+    [TextArea]
     public string failText;
 
+    [TextArea]
     public string characterCriticalFail;
+    [TextArea]
     public string partyCriticalFail;
 
     public E_Trait traitToPass;
+    
 }
