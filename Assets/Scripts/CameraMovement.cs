6a3d30b6f5f7e4e97fc77405d68d133ed2280753
JEMMA GILBERT <jg218537@falmouth.ac.uk>
2021-02-23 15:18:18 +0000

    Added:
    - Bob script
    - Camera movement script
    
    Signed-off-by: JEMMA GILBERT <jg218537@falmouth.ac.uk>

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public int speed = 100;
+
+    void Update()
+    { 
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+        mousePos.z = 0;
+        mousePos.x = -mousePos.x * 0.03f;
+        mousePos.y = -mousePos.y * 0.03f + 1;
+        transform.position = Vector3.Lerp(transform.position, mousePos, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerBob.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBob : MonoBehaviour
+{
+    private Vector3 pos;
+
+    public Vector3 upPos, downPos;
+
+    public int delay;
+
+    void Start()
+    {
+        pos = transform.position;
+        StartCoroutine(Bob());
+    }
+
+    IEnumerator Bob()
+    {
+        yield return new WaitForSeconds(delay);
+
+        jump:
+
+        transform.position = new Vector3(upPos.x, upPos.y, upPos.z);
+        yield return new WaitForSeconds(1);
+        transform.position = new Vector3(pos.x, pos.y, pos.z);
+        yield return new WaitForSeconds(1);
+        transform.position = new Vector3(downPos.x, downPos.y, downPos.z);
+        yield return new WaitForSeconds(1);
+        transform.position = new Vector3(pos.x, pos.y, pos.z);
+        yield return new WaitForSeconds(1);
+
+        goto jump;
+    }
+}
