c7ec761931b3ede46c0f754929ecbe90efdfc66c
Stamperton <35846593+Stamperton@users.noreply.github.com>
2021-02-24 17:01:19 +0000

    UI Health/Stam Done

---
+++ Assets/ArtCycle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArtCycle : MonoBehaviour
+{
+    Image thisImage;
+
+    public Sprite[] availableSprites;
+
+    private void Start()
+    {
+        thisImage = GetComponent<Image>();
+    }
+
+    void ChooseNewSprite()
+    {
+        Sprite _temp = availableSprites[Random.Range(0, availableSprites.Length)];
+
+        thisImage.sprite = _temp;
+    }
+}
